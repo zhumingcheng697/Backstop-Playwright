f3d0bf6e7430e6ffece1f8daec88b9599d719e40
McCoy Zhu <mz1945@nyu.edu>
2020-11-16 19:05:23 -0800

    FIX: ignoreCSP should be working now

--- capture/engine_scripts/playwright/ignoreCSP.js
+++ capture/engine_scripts/playwright/ignoreCSP.js
@@ -27,39 +27,33 @@ const agent = new https.Agent({
 });
 
 module.exports = async function (page, scenario) {
-  const intercept = async (request, targetUrl) => {
+  const intercept = async (route, request) => {
     const requestUrl = request.url();
 
-    // FIND TARGET URL REQUEST
-    if (requestUrl === targetUrl) {
-      const cookiesList = await page.cookies(requestUrl);
-      const cookies = cookiesList.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
-      const headers = Object.assign(request.headers(), { cookie: cookies });
-      const options = {
-        headers: headers,
-        body: request.postData(),
-        method: request.method(),
-        follow: 20,
-        agent
-      };
+    const cookiesList = await page.context().cookies([requestUrl]);
+    const cookies = cookiesList.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
+    const headers = Object.assign(request.headers(), { cookie: cookies });
+    const options = {
+      headers: headers,
+      body: request.postData(),
+      method: request.method(),
+      follow: 20,
+      agent
+    };
 
-      const result = await fetch(requestUrl, options);
+    const result = await fetch(requestUrl, options);
 
-      const buffer = await result.buffer();
-      let cleanedHeaders = result.headers._headers || {};
-      cleanedHeaders['content-security-policy'] = '';
-      await request.respond({
-        body: buffer,
-        headers: cleanedHeaders,
-        status: result.status
-      });
-    } else {
-      request.continue();
-    }
+    const buffer = await result.buffer();
+    let cleanedHeaders = result.headers._headers || {};
+    cleanedHeaders['content-security-policy'] = '';
+    await route.fulfill({
+      body: buffer,
+      headers: cleanedHeaders,
+      status: result.status
+    });
   };
 
-  await page.setRequestInterception(true);
-  page.on('request', req => {
-    intercept(req, scenario.url);
+  page.route(scenario.url, (route, request) => {
+    intercept(route, request);
   });
 };
